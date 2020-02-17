26be45c3f22567513c81118feb03771bf92ab181
ganargatul <ganar.gatul@gmail.com>
2020-02-17 18:29:52 +0700

    Change icon bottom navigation and Add menu

--- app/src/main/java/com/scc/bukusakuonline/ui/MainActivity.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.scc.bukusakuonline.ui;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewInner;
 import com.scc.bukusakuonline.R;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationViewInner navView = findViewById(R.id.nav_view);
+//        navView.seticon
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_asd)
+                R.id.navigation_home, R.id.navigation_history,R.id.navigation_dashboard,R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- app/src/main/java/com/scc/bukusakuonline/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.scc.bukusakuonline.ui.notifications;
+package com.scc.bukusakuonline.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,25 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.scc.bukusakuonline.R;
 
-public class NotificationsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private ProfileViewModel profileViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        profileViewModel =
+                ViewModelProviders.of(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+        profileViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
