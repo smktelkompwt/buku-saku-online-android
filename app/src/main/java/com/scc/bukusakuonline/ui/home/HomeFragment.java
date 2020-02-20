3d6619e8db56e9af3111ba9800b2e90f0e817f88
ganargatul <ganar.gatul@gmail.com>
2020-02-20 18:09:03 +0700

    fix error in detail pelanggaran bug on bottomnavigation if dashboard active fix toolbar hide in homefragment

--- app/src/main/java/com/scc/bukusakuonline/ui/home/HomeFragment.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/home/HomeFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -25,5 +26,16 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         return root;
+    }
+    //to hide toolbar if conflict please resolve
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
 }
