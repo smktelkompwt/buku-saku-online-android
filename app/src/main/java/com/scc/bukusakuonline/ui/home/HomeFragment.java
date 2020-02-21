ac8c14f075fc350f93933d858d6db66fb891ab19
Bagas Makhali <makhalibagas1@gmail.com>
2020-02-21 11:14:40 +0700

    redesign searchview in fragment_home.xml

--- app/src/main/java/com/scc/bukusakuonline/ui/home/HomeFragment.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/home/HomeFragment.java
@@ -19,17 +19,12 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-<<<<<<< HEAD
         SearchView searchView = root.findViewById(R.id.sercing);
-        searchView.setQueryHint("Cari Nama Berdasarkan NIS");
-=======
->>>>>>> b71da54852f63f097f12f5e77698192b877d345c
+
         return root;
     }
 }
