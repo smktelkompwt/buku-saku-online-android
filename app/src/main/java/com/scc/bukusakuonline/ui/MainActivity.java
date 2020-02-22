74d7bcb650fc5be49f85c629d501b41deded6af4
YudhanJeffri <yudhanjeffri168@gmail.com>
2020-02-22 22:10:20 +0700

    add feature complaint

--- app/src/main/java/com/scc/bukusakuonline/ui/MainActivity.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity  {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navigation, navController);
+        NavigationUI.setupWithNavController(navigation, navController);
 
     }
-}
+
+    }
--- app/src/main/java/com/scc/bukusakuonline/ui/pengaduan/PengaduanFragment.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/pengaduan/PengaduanFragment.java
@@ -8,13 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.scc.bukusakuonline.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PengaduanFragment extends Fragment {
+public class PengaduanFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
 
     public PengaduanFragment() {
@@ -26,7 +34,34 @@ public class PengaduanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pengaduan, container, false);
+        View v = inflater.inflate(R.layout.fragment_pengaduan, container, false);
+        Spinner spinner = v.findViewById(R.id.spinner_kategori);
+        Spinner spinner_pasal = v.findViewById(R.id.spinner_pasal);
+
+        ArrayAdapter<CharSequence> adapter_pasal = ArrayAdapter.createFromResource(getContext(),
+                R.array.spinner_pasal, android.R.layout.simple_spinner_item);
+        adapter_pasal.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner_pasal.setAdapter(adapter_pasal);
+        spinner_pasal.setOnItemSelectedListener(this);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.spinner_kategori, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        return v;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
