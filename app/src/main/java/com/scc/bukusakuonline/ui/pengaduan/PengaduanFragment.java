6b5ed6659b8f57772b2755d691f838322ba42a5e
YudhanJeffri <yudhanjeffri168@gmail.com>
2020-02-24 13:20:06 +0700

    add feature-complaint & make code much more readable

--- app/src/main/java/com/scc/bukusakuonline/ui/pengaduan/PengaduanFragment.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/pengaduan/PengaduanFragment.java
@@ -2,28 +2,33 @@ package com.scc.bukusakuonline.ui.pengaduan;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.jaredrummler.materialspinner.MaterialSpinner;
+import androidx.fragment.app.Fragment;
+
 import com.scc.bukusakuonline.R;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PengaduanFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
+
+    @BindView(R.id.spinner_kategori)
+    Spinner spinnerKategori;
+    @BindView(R.id.spinner_pasal)
+    Spinner spinnerPasal;
 
     public PengaduanFragment() {
         // Required empty public constructor
@@ -35,23 +40,20 @@ public class PengaduanFragment extends Fragment implements AdapterView.OnItemSel
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_pengaduan, container, false);
-        Spinner spinner = v.findViewById(R.id.spinner_kategori);
-        Spinner spinner_pasal = v.findViewById(R.id.spinner_pasal);
+        ButterKnife.bind(this, v);
 
         ArrayAdapter<CharSequence> adapter_pasal = ArrayAdapter.createFromResource(getContext(),
                 R.array.spinner_pasal, android.R.layout.simple_spinner_item);
         adapter_pasal.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
-        spinner_pasal.setAdapter(adapter_pasal);
-        spinner_pasal.setOnItemSelectedListener(this);
-
-
+        spinnerPasal.setAdapter(adapter_pasal);
+        spinnerPasal.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
                 R.array.spinner_kategori, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        spinnerKategori.setAdapter(adapter);
+        spinnerKategori.setOnItemSelectedListener(this);
         return v;
     }
 
@@ -63,5 +65,15 @@ public class PengaduanFragment extends Fragment implements AdapterView.OnItemSel
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @OnClick(R.id.imageButton3)
+    public void onImageButton3Clicked() {
+        Toast.makeText(getContext(),"Upload Bukti",Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.button)
+    public void onButtonClicked() {
+        Toast.makeText(getContext(),"Laporkan",Toast.LENGTH_SHORT).show();
     }
 }
