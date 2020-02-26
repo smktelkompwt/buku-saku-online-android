1f5f80bc4bd68ffc1393ddfa557b9f5093a4aa71
Bagas Makhali <makhalibagas1@gmail.com>
2020-02-26 19:38:52 +0700

    membuat halaman layout

--- app/src/main/java/com/scc/bukusakuonline/ui/DetailPeraturanActivity.java
+++ app/src/main/java/com/scc/bukusakuonline/ui/DetailPeraturanActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 
 import com.scc.bukusakuonline.R;
 
+import java.util.Objects;
+
 public class DetailPeraturanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_peraturan);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.detail_1));
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
