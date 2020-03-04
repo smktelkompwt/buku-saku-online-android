dd606799963ee03dbac1b403de149084a76e0dfb
ganargatul <ganar.gatul@gmail.com>
2020-03-04 19:31:24 +0700

    add kotlin integrasi api detailpoint rev detail point xml

---
+++ app/src/main/java/com/scc/bukusakuonline/model/DetailPointItems.kt
@@ -0,0 +1,12 @@
+package com.scc.bukusakuonline.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DetailPointItems(
+        @SerializedName("kode")
+        val code : String ?,
+        @SerializedName("jenis_pelanggaran")
+        val jenis_pelanggaran : String ?,
+        @SerializedName("point")
+        val point : String ?
+)
