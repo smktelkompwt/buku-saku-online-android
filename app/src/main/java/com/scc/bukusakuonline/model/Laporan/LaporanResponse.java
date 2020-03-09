7992606aea4f1bb4153f5f0dee0f60c283101256
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2020-03-09 10:48:37 +0700

    move branch

---
+++ app/src/main/java/com/scc/bukusakuonline/model/Laporan/LaporanResponse.java
@@ -0,0 +1,53 @@
+package com.scc.bukusakuonline.model.Laporan;
+
+import java.util.List;
+
+public class LaporanResponse{
+	private int code;
+	private List<DataItem> data;
+	private String success;
+	private String message;
+
+	public void setCode(int code){
+		this.code = code;
+	}
+
+	public int getCode(){
+		return code;
+	}
+
+	public void setData(List<DataItem> data){
+		this.data = data;
+	}
+
+	public List<DataItem> getData(){
+		return data;
+	}
+
+	public void setSuccess(String success){
+		this.success = success;
+	}
+
+	public String getSuccess(){
+		return success;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"LaporanResponse{" + 
+			"code = '" + code + '\'' + 
+			",data = '" + data + '\'' + 
+			",success = '" + success + '\'' + 
+			",message = '" + message + '\'' + 
+			"}";
+		}
+}
