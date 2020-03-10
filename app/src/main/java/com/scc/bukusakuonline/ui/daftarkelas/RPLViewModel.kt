b4c47fe2e611acd8ac82fba32f2c04004b2a8229
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2020-03-10 15:37:38 +0700

    adapter siswa

--- app/src/main/java/com/scc/bukusakuonline/ui/daftarkelas/RPLViewModel.kt
+++ app/src/main/java/com/scc/bukusakuonline/ui/daftarkelas/RPLViewModel.kt
@@ -9,11 +9,32 @@ import com.scc.bukusakuonline.connection.ApiService
 import com.scc.bukusakuonline.connection.RetroConfig
 import com.scc.bukusakuonline.model.DetailPointItems
 import com.scc.bukusakuonline.model.DetailPointResponse
+import com.scc.bukusakuonline.model.siswa.SiswaKelasItem
+import com.scc.bukusakuonline.model.siswa.SiswaKelasResponse
 import retrofit2.Call
 import retrofit2.Response
 
 class RPLViewModel : ViewModel() {
 
+    private  var listSiswa : MutableLiveData<List<SiswaKelasItem>> = MutableLiveData()
 
+
+    fun loadData(context: Context){
+        Log.d("viewmodel", "viewmodel")
+        val sharedPreferences = context.getSharedPreferences("PREF", Context.MODE_PRIVATE)
+        val token ="Bearer "+ sharedPreferences.getString("TOKEN","abc")
+        RetroConfig.getRetrofit().create(ApiService::class.java).getSiswa(token,"isi apa?? kan aku wis get  spinner e isi kelas").enqueue(object : retrofit2.Callback<SiswaKelasResponse>{
+            override fun onFailure(call: Call<SiswaKelasResponse>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call<SiswaKelasResponse>, response: Response<SiswaKelasResponse>) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+
+        })
+    }
+    val listData: LiveData<List<SiswaKelasItem>> = listSiswa
 
 }
